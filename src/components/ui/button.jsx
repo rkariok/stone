f489499a784ef78642999d4ae9295f3d0809fe8b
rkariok <roy@roykariok.com>
2025-05-16 10:53:28 -0700

    Delete src directory

--- src/components/ui/button.jsx
+++
@@ -1,3 +0,0 @@
-export function Button({ children, ...props }) {
-  return <button className="bg-black text-white px-4 py-2 rounded" {...props}>{children}</button>;
-}
--- src/components/ui/input.jsx
+++
@@ -1,3 +0,0 @@
-export function Input({ type = 'text', ...props }) {
-  return <input type={type} className="border rounded px-2 py-1 w-full" {...props} />;
-}
