b5477a18ae09312871a7ee6892641eface4a9dc6
rkariok <roy@roykariok.com>
2025-05-16 08:10:32 -0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import StoneTopEstimator from './StoneTopEstimator'; import './index.css'; ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><StoneTopEstimator /></React.StrictMode>);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './style.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
